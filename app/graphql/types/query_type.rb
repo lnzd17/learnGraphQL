781b98aaf8a48f87a6535eee80706b8f0fa4f070
lnzd17 <lindsaykayedutton@gmail.com>
2018-04-15 14:58:16 -0400

    first commit

---
+++ app/graphql/types/query_type.rb
@@ -0,0 +1,17 @@
+Types::QueryType = GraphQL::ObjectType.define do
+  name 'Query'
+
+  # queries are just represented as fields
+  field :allLinks, !types[Types::LinkType] do
+    # resolve would be called in order to fetch data for that field
+    resolve -> (obj, args, ctx) { Link.all }
+  end
+
+  field :allUsers, !types[Types::UserType] do
+    resolve -> (obj, args, ctx) { User.all }
+  end
+
+  field :allVotes, !types[Types::VoteType] do
+    resolve -> (obj, args, ctx) { Vote.all }
+  end
+end
